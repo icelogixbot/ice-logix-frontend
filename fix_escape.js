cf71f4f19bd95167b6ca523dfea72765e6801171
Kirill Bogdanovich <kirillbog2anovich@gmail.com>
2026-07-29 20:34:28 +0300

    chore: clean up temp files, move to _archive, update .gitignore

--- fix_escape.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('index.html', 'utf8');
-c = c.replace(/\\`/g, '`');
-c = c.replace(/\\\$/g, '$');
-fs.writeFileSync('index.html', c, 'utf8');
-console.log('Fixed escaped backticks and dollars!');
